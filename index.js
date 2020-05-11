e3b4b10c2b84b2fb4c49edac1249030b74873338
Jacqueline Nyiraturatsinze <nturatsinzy@yahoo.fr>
2020-05-11 16:26:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  return animal
+  return animal = 'cat';
 }
 const two = 2;
 function add2(n) {
