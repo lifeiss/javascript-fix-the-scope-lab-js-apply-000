199d355749a71cc92022f86dd647e3cabc1970f5
Jacqueline Nyiraturatsinze <nturatsinzy@yahoo.fr>
2020-05-11 16:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   return animal = 'cat';
+  return animal;
 }
 const two = 2;
 function add2(n) {
